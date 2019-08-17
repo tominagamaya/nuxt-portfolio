c148b9ced64e9141fb16962eccbd6b2e2d292609
tominagamaya <maya.tominaga.9@gmail.com>
2019-08-17 19:02:58 +0900

    Add pages

---
+++ layouts/error.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="layout-error">
+    <section class="error">
+      <h2>404</h2>
+      <div class="article-body">
+        <h3>ページをひらけません</h3>
+        <p>アクセスされたページは存在しないか、すでに削除されています。</p>
+      </div>
+    </section>
+  </div>
+</template>
---
+++ pages/about/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page-about">
+    <section class="about">
+      <h2>はじめまして</h2>
+      <div class="article-body">
+        <p>Maya Tominaga</p>
+        <p>SIerからWeb系のエンジニア</p>
+        <p>週1で技術ブログを執筆</p>
+        <p>Java / Python / Vue.js / GCP</p>
+      </div>
+    </section>
+    <section class="history">
+      <h2>経歴</h2>
+      <div class="article-body">
+        <p>新卒から約4年間はSIerとして官公庁等のシステム開発を担当</p>
+        <p>常駐派遣としてWeb系の企業に2年間ほど勤務</p>
+        <p>アナリティクス事業会社でAIを組み込んだWebアプリの開発を担当</p>
+      </div>
+    </section>
+  </div>
+</template>
