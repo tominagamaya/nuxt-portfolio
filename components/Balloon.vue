a412a4af8eddf0f556a56ec2e7230dec020a4305
tominagamaya <maya.tominaga.9@gmail.com>
2019-08-28 20:58:41 +0900

    Update shake animation

--- components/Balloon.vue
+++ components/Balloon.vue
@@ -1,60 +1,28 @@
 <template>
-  <img
-    class="img-balloon"
-    src="~/assets/images/balloon.svg"
-    :style="{
-      transform: `translate(${x + dx}px, ${y + dy}px) 
-      scale(${scaleX}, ${scaleY}) rotate(${rotate}deg)`,
-      transition: `transform ${duration}ms ${easing}`
-    }"
-    @click="up(50)"
-  />
+  <img class="img-balloon shake" src="~/assets/images/balloon.svg" />
 </template>
 <script>
-export default {
-  data() {
-    return {
-      dx: 0,
-      dy: 0,
-      duration: 1000,
-      easing: 'ease'
-    }
-  },
-  props: {
-    x: { type: Number, default: -150 },
-    y: { type: Number, default: 150 },
-    scaleX: { type: Number, default: 1.0 },
-    scaleY: { type: Number, default: 1.0 },
-    rotate: { type: Number, default: 0 }
-  },
-  methods: {
-    up(height) {
-      this.dy -= height
-      this.easing = 'ease-out'
-      window.setTimeout(() => {
-        this.dy = 0
-        this.easing = 'ease-in'
-      }, this.duration)
-    }
-  }
-}
+export default {}
 </script>
 <style scoped lang="scss">
 .img-balloon {
   position: absolute;
-  top: 0;
-  right: 0;
+  top: 150px;
+  right: 150px;
   width: 100px;
-  will-change: transform;
 }
 .shake {
-  animation: animationShake 1s steps(6) infinite;
+  animation: animationShake 4s infinite;
 }
 @keyframes animationShake {
-  from {
+  0% {
+    transform: rotate(15deg);
   }
-  to {
-    rotate: -45;
+  50% {
+    transform: rotate(-15deg);
+  }
+  100% {
+    transform: rotate(15deg);
   }
 }
 </style>
