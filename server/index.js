fcb71222b73335d31e155321c13ff71ef53023f2
tominagamaya <maya.tominaga.9@gmail.com>
2019-10-12 16:42:19 +0900

    Update ssl support

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
+const https = require('https')
+const fs = require('fs')
+
 const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
+
+// const isProd = (process.env.NODE_ENV === 'production')
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
@@ -20,6 +25,12 @@ async function start() {
   } else {
     await nuxt.ready()
   }
+
+  const options = {
+    key: fs.readFileSync('./server/server.key'),
+    cert: fs.readFileSync('./server/server.crt')
+  }
+  https.createServer(options, nuxt.render).listen(port)
 
   // Give nuxt middleware to express
   app.use(nuxt.render)
